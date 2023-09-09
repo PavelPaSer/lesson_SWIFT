94d836558d1ce3270948fe09a8bd303b15f1ee8b
«Pashkov <pashkov921326@gmail.com>
2023-09-09 19:06:28 +0300

    first commit

--- Lesson_4/task.swift
+++ Lesson_4/task.swift
@@ -1,7 +1,7 @@
 
 //1. Создать перечисление с видами пиццы (хотя бы 4 - 5 кейсов)
 
-enum PizzaType {
+enum pizzaType {
     case margherita
     case pepperoni
     case vegetaria
@@ -12,54 +12,54 @@ enum PizzaType {
 //2. Создать структуру пиццы, она должна содержать стоимость, вид пиццы, толстое или тонкое тесто и добавки (например, дополнительно добавить пепперони, помидоры, сыр). Вид пиццы должен быть вложенным типом для структуры пиццы. Подсказка: добавки лучше также сделать перечислением.
 // Перечисление для добавок
 
-enum Topping {
+enum topping {
     case ketchup
     case tomatoes
     case cheese
 }
 
-struct Pizza {
-    let pizzaType: PizzaType
+struct pizza {
+    let pizzaType: pizzaType
     let crustType: String // Толстое или тонкое тесто
-    let toppings: [Topping] // Массив выбранных добавок
+    let toppings: [topping] // Массив выбранных добавок
     let price: Double // Стоимость
 }
     
 //4. Создать класс пиццерии, добавить массив с возможными пиццами. Переменная с массивом должна быть приватной. Массив задается в инициализаторе.
-class PizzaShop {
-    private var availablePizzas: [Pizza]
+class pizzaShop {
+    private var availablePizzas: [pizza]
     init() {
         availablePizzas = [
-            Pizza(pizzaType:
+            pizza(pizzaType:
                 .margherita, crustType: "Маргарита с кетчупом и сыром", toppings: [.tomatoes, .cheese], price: 455.99),
-            Pizza(pizzaType: .pepperoni, crustType: "Пепирони с кетчупом и сыром", toppings:
+            pizza(pizzaType: .pepperoni, crustType: "Пепирони с кетчупом и сыром", toppings:
                     [.ketchup, .cheese], price: 789),
-            Pizza(pizzaType: .vegetaria, crustType: "Вигитаринская пицца с кетчупом и сыром", toppings:
+            pizza(pizzaType: .vegetaria, crustType: "Вигитаринская пицца с кетчупом и сыром", toppings:
                     [.tomatoes, .cheese], price: 657),
-            Pizza(pizzaType: .forcheeses, crustType: "Четыре Сыра с томатом",           toppings: [.tomatoes], price: 1300),
-            Pizza(pizzaType: .carbonara, crustType: "Карбонара с кетчупом и сыром",     toppings:
+            pizza(pizzaType: .forcheeses, crustType: "Четыре Сыра с томатом",           toppings: [.tomatoes], price: 1300),
+            pizza(pizzaType: .carbonara, crustType: "Карбонара с кетчупом и сыром",     toppings:
                     [.ketchup, .cheese], price: 590.90)
         ]
     }
     //3. Написать в классе пиццерии функции для добавления новой пиццы и для получения всех доступных пицц
     
-    func addPizza(pizza: Pizza) {
+    func addPizza(pizza: pizza) {
         availablePizzas.append(pizza)
     }
     
-    func getAllPizzas() -> [Pizza] {
+    func getAllPizzas() -> [pizza] {
         return availablePizzas
     }
 }
 
 // 5.Создать экземпляр класса пиццерии и добавить в него несколько пицц
-let pizzaShop = PizzaShop()
+let pizzaShopp = pizzaShop()
 
-let margheritaNew = Pizza(pizzaType: .margherita, crustType: "Маргарита Древняя", toppings: [.tomatoes, .cheese], price: 690.90)
-let pepperoniNew = Pizza(pizzaType: .pepperoni, crustType: "Пепирони Греческая", toppings: [.ketchup, .cheese], price: 590.90)
+let margheritaNew = pizza(pizzaType: .margherita, crustType: "Маргарита Древняя", toppings: [.tomatoes, .cheese], price: 690.90)
+let pepperoniNew = pizza(pizzaType: .pepperoni, crustType: "Пепирони Греческая", toppings: [.ketchup, .cheese], price: 590.90)
 
-pizzaShop.addPizza(pizza: margheritaNew)
-pizzaShop.addPizza(pizza: pepperoniNew)
+pizzaShopp.addPizza(pizza: margheritaNew)
+pizzaShopp.addPizza(pizza: pepperoniNew)
 
 
 
